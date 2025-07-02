b4def02b8ed8340458bf109008fdfe362c232f67
MHamzaAamir <121198646+MHamzaAamir@users.noreply.github.com>
2025-07-02 21:23:51 +0500

    fixed bugs

--- app/components/herosection.jsx
+++ app/components/herosection.jsx
@@ -3,19 +3,17 @@ import Link from "next/link";
 import Image from 'next/image';
 const Herosection = () => {
   return (
-    <div className="bg-yellow-200 w-screen h-screen bg-cover  flex items-center justify-center mt-0 shadow-lg">
+    <div className="bg-yellow-200 w-screen h-screen bg-cover flex items-center justify-center mt-0 shadow-lg">
        <div className="w-screen flex justify-center items-center relative">
-        <div className="w-[300px] ">
+        <div className="w-[210px] ">
             <Image src={"/hero.png"} width={640} height={1280} alt={"myimage"}/>
 
         </div>
         
-        
-        
-         <div className=" absolute  top-[40%] flex flex-col ">
-            <div className="-translate-y-[-10%] px-[37%] text-shadow-2xs font-semibold text-3xl text-white">  
-                Hand Crafted
-                </div>
+         <div className=" absolute  top-[50%] -translate-y-[50%] flex flex-col justify-center items-center">
+            <div className="text-shadow-2xs font-semibold text-5xl text-white">  
+              Hand Crafted
+            </div>
             <div className="font-extrabold text-white text-[12rem]"> 
                 Ice Cream
             </div>
--- app/page.jsx
+++ app/page.jsx
@@ -8,9 +8,9 @@ export default function Home() {
   return (
    
     <>
-      <Navbar/>,
-      <Herosection/>,
-      <Menu/>,
+      <Navbar/>
+      <Herosection/>
+      <Menu/>
     </>
     
 
